28d58d476c180d6e4e047bc03b24eca518b28ca7
Richard Challis <rjchallis@gmail.com>
2015-12-07 14:41:16 +0000

    testing

--- lib/sequenceserver/hierarchy.rb
+++ lib/sequenceserver/hierarchy.rb
@@ -1,33 +1,32 @@
 module SequenceServer
   # Module to contain methods for dealing with taxonomic hierarchy.
-  module Hierarchy
-  
-  	def parents
-      {
-        :default => 'Lepidoptera',
-        :Agraulis => 'Butterflies',
-  	    :Bicyclus => 'Butterflies',
-  	    :Bombyx => 'Moths',
-  	    :Chilo => 'Moths',
-  	    :Danaus => 'Butterflies',
-  	    :Eueides => 'Butterflies',
-  	    :Heliconius => 'Butterflies',
-  	    :Laparus => 'Butterflies',
-  	    :Lerema => 'Butterflies',
-  	    :Manduca => 'Moths',
-  	    :Melitaea => 'Butterflies',
-  	    :Neruda => 'Butterflies',
-  	    :Papilio => 'Butterflies',
-  	    :Pieris => 'Butterflies',
-  	    :Plodia => 'Moths',
-  	    :Plutella => 'Moths'
-  	  }
+  Hierarchy = Struct.new(:parents,:root) do
+
+    def parents
+        {
+          :default => 'Lepidoptera',
+          :Agraulis => 'Butterflies',
+    	    :Bicyclus => 'Butterflies',
+    	    :Bombyx => 'Moths',
+    	    :Chilo => 'Moths',
+    	    :Danaus => 'Butterflies',
+    	    :Eueides => 'Butterflies',
+    	    :Heliconius => 'Butterflies',
+    	    :Laparus => 'Butterflies',
+    	    :Lerema => 'Butterflies',
+    	    :Manduca => 'Moths',
+    	    :Melitaea => 'Butterflies',
+    	    :Neruda => 'Butterflies',
+    	    :Papilio => 'Butterflies',
+    	    :Pieris => 'Butterflies',
+    	    :Plodia => 'Moths',
+    	    :Plutella => 'Moths'
+    	  }
+      end
+
+      def root
+        'Lepidoptera'
+      end
     end
-    
-    def root
-      'Lepidoptera'
-    end
-    
   end
 end
-
