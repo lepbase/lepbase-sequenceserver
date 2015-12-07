932fc4e2876e7c2c03e28b3812ca482d980569b7
Richard Challis <rjchallis@gmail.com>
2015-12-07 14:28:47 +0000

    use => not =

--- lib/sequenceserver/hierarchy.rb
+++ lib/sequenceserver/hierarchy.rb
@@ -4,23 +4,23 @@ module SequenceServer
   
   	def parents
       {
-        :default = 'Lepidoptera',
-        :Agraulis = 'Butterflies',
-  	    :Bicyclus = 'Butterflies',
-  	    :Bombyx = 'Moths',
-  	    :Chilo = 'Moths',
-  	    :Danaus = 'Butterflies',
-  	    :Eueides = 'Butterflies',
-  	    :Heliconius = 'Butterflies',
-  	    :Laparus = 'Butterflies',
-  	    :Lerema = 'Butterflies',
-  	    :Manduca = 'Moths',
-  	    :Melitaea = 'Butterflies',
-  	    :Neruda = 'Butterflies',
-  	    :Papilio = 'Butterflies',
-  	    :Pieris = 'Butterflies',
-  	    :Plodia = 'Moths',
-  	    :Plutella = 'Moths'
+        :default => 'Lepidoptera',
+        :Agraulis => 'Butterflies',
+  	    :Bicyclus => 'Butterflies',
+  	    :Bombyx => 'Moths',
+  	    :Chilo => 'Moths',
+  	    :Danaus => 'Butterflies',
+  	    :Eueides => 'Butterflies',
+  	    :Heliconius => 'Butterflies',
+  	    :Laparus => 'Butterflies',
+  	    :Lerema => 'Butterflies',
+  	    :Manduca => 'Moths',
+  	    :Melitaea => 'Butterflies',
+  	    :Neruda => 'Butterflies',
+  	    :Papilio => 'Butterflies',
+  	    :Pieris => 'Butterflies',
+  	    :Plodia => 'Moths',
+  	    :Plutella => 'Moths'
   	  }
     end
     
