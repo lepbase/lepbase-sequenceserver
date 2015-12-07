a29fd1043f050ffb34c7f9aa57bf74057086192b
Richard Challis <rjchallis@gmail.com>
2015-12-07 15:35:45 +0000

    testing

--- lib/sequenceserver/routes.rb
+++ lib/sequenceserver/routes.rb
@@ -108,7 +108,7 @@ target="#{target}">)
     get '/' do
       @input_sequence = ""
       erb :search, :locals => { :databases => Database.group_by(&:type),
-                                :hierarchy => Hierarchy.new('Lepidoptera') }
+                                :hierarchy => Hierarchy.root }
     end
 
     
