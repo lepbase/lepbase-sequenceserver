bdf92e058c7d3475ffe9ba12fdcdac4982779e07
Richard Challis <rjchallis@gmail.com>
2015-12-08 10:10:36 +0000

    add methods to process hierarchy

--- lib/sequenceserver/hierarchy.rb
+++ lib/sequenceserver/hierarchy.rb
@@ -8,22 +8,22 @@ module SequenceServer
     
   	def parents
       par = {
-        'agraulis' => 'Butterflies',
-  	    'bicyclus' => 'Butterflies',
-  	    'bombyx' => 'Moths',
-  	    'chilo' => 'Moths',
-  	    'danaus' => 'Butterflies',
-  	    'eueides' => 'Butterflies',
-  	    'heliconius' => 'Butterflies',
-  	    'laparus' => 'Butterflies',
-  	    'lerema' => 'Butterflies',
-  	    'manduca' => 'Moths',
-  	    'melitaea' => 'Butterflies',
-  	    'neruda' => 'Butterflies',
-  	    'papilio' => 'Butterflies',
-  	    'pieris' => 'Butterflies',
-  	    'plodia' => 'Moths',
-  	    'plutella' => 'Moths'
+        'Agraulis' => 'Butterflies',
+  	    'Bicyclus' => 'Butterflies',
+  	    'Bombyx' => 'Moths',
+  	    'Chilo' => 'Moths',
+  	    'Danaus' => 'Butterflies',
+  	    'Eueides' => 'Butterflies',
+  	    'Heliconius' => 'Butterflies',
+  	    'Laparus' => 'Butterflies',
+  	    'Lerema' => 'Butterflies',
+  	    'Manduca' => 'Moths',
+  	    'Melitaea' => 'Butterflies',
+  	    'Neruda' => 'Butterflies',
+  	    'Papilio' => 'Butterflies',
+  	    'Pieris' => 'Butterflies',
+  	    'Plodia' => 'Moths',
+  	    'Plutella' => 'Moths'
   	  }
   	  par.default = @root
   	  return par
@@ -33,10 +33,44 @@ module SequenceServer
       arr = [name]
       until name == @root
         name = parents[name]
-        arr << name
+        arr.unshift name
       end
       return arr
     end
+    
+    def nested_list(databases)
+      hash = {}
+      databases.each do |database|
+      	title = database.title or database.name
+        name = title.split.first.capitalize
+        type = database.type
+        levels = nesting(name)
+        parent = 'root'
+        levels.each do |level|
+          hash[parent] ||= []
+          if level == name
+            db = database.id
+          else
+            db = false
+          end
+          hash[parent].push {:name => level, :db => db, :type => type}
+          parent = level
+        end
+      end
+      return hash
+    end
+    
+    #def ul(databases)
+    #  hash = nested_list(databases)
+    #  hash['root'].each do |level|
+    #    puts key
+    #    value.each do |k,v|
+    #      puts k
+    #      puts v
+    #    end
+    #  end
+    #  return hash
+    #end
     
   end
 end
