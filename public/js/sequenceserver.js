67c02c6132d0d8bc6abfcdb86471a8133b666959
Richard Challis <rjchallis@gmail.com>
2015-12-10 13:56:26 +0000

    work around items not being in the DOM when nodes are collapsed

--- public/js/sequenceserver.js
+++ public/js/sequenceserver.js
@@ -841,7 +841,21 @@ $(document).ready(function(){
         $('#spinner').modal();
 
         // add hidden fields to form
-        var ids = $('#transcripts').jstree("get_selected")
+        $('.treediv').each(function(){
+        	mytree = $(this);
+        	var ids = mytree.jstree("get_selected");
+        	ids.forEach(function(id,i){
+        		mynode = mytree.jstree('get_node',id,true);
+        		if (mynode.li_attr.value){
+        			$('<input />').attr('type', 'hidden')
+					.attr('name', mynode.li_attr.name)
+					.attr('value', mynode.li_attr.value)
+					.appendTo('#blast');
+        		}
+			});
+        });
+        
+       /* var ids = $('#transcripts').jstree("get_selected")
                   .concat($('#scaffolds').jstree("get_selected"))
                   .concat($('#proteins').jstree("get_selected"));
         
@@ -852,7 +866,7 @@ $(document).ready(function(){
 					.attr('value', $('#'+id).attr('value'))
 					.appendTo('#blast');
 			}
-		});
+		});*/
 
         // BLAST now
         
